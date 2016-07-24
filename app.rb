d00c6db0821f03927efa51f886e15ff7768f4703
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-07-24 11:33:25 -0700

    add working routes for adding, editing, and deleting new stores

--- app.rb
+++ app.rb
@@ -8,12 +8,17 @@ get('/') do
   erb(:index)
 end
 
+get('/stores/new') do
+  erb(:store_form)
+end
+
 get('/autogenerate') do
   footlocker = Store.create(name: 'Foot Locker', location: '9459 SW Washington St, Tigard, OR 97223', phone: '503-684-2053', open_time: '9', close_time: '8');
   champs = Store.create(name: 'Champs', location: '12000 SE 82nd Ave, Happy Valley, OR 97086', phone: '503-772-9012', open_time: '7', close_time: '10');
   adidas = Brand.create(name: 'Adidas', image: 'http://logos-download.com/wp-content/uploads/2016/03/Adidas_Originals_logo.png')
   nike = Brand.create(name: 'Nike', image: 'http://www.screenitltd.com/sites/default/files/brands/nike-logo.png')
   timberland = Brand.create(name: 'Timberland', image: 'http://images.theexecutiveadvertising.com/images/manflogo/timberland.png')
+  reebock = Brand.create(name: 'Reebock', image: 'http://www.findthatlogo.com/wp-content/uploads/2011/09/reebok-logo.jpg')
   footlocker.brands.push(nike)
   footlocker.brands.push(timberland)
   champs.brands.push(adidas)
@@ -28,14 +33,14 @@ get('/stores/:id/edit') do
 end
 
 post('/stores/new') do
-  @store = Store.create(name: params['name_store'], location: params['location'], phone: params['phone'], open_time: params['open_time'], close_time: params['close_time']);
-  redirect "stores/#{@store.id()}"
+  @store = Store.create(name: params['name'], location: params['location'], phone: params['phone'], open_time: params['open_time'], close_time: params['close_time']);
+  redirect "/"
 end
 
 patch('/stores/:id') do
   @store = Store.find(params['id'])
-  @store.update(name: params['store_name'], location: params['location'], phone: params['phone'], open_time: params['open_time'], close_time: params['close_time'])
-  redirect "stores/#{@store.id()}/stores/edit"
+  @store.update(name: params['name'], location: params['location'], phone: params['phone'], open_time: params['open_time'], close_time: params['close_time'])
+  redirect "stores/#{@store.id()}/edit"
 end
 
 delete ('/stores/:id/delete') do
@@ -50,12 +55,6 @@ post('/brands/new') do
   else
     brand = Brand.create(name: params['brand_name'], image: params['image'])
   end
-  @Store.brands.push(brand)
-  redirect "stores/#{params['store_id']}/brands/edit"
-end
-
-patch('/stores/:store_id/brands/:id') do
-  @brand = Brand.find(params['id'])
-  @brand.update(store_ids: nil)
-  redirect "/stores/#{params['store_id']}/brands/edit"
+  @store.brands.push(brand)
+  redirect "stores/#{params['store_id']}/edit"
 end
